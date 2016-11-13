a782b6ab13d933d9e40b6be1284c910e154d4509
Ben <ben@kraal.se>
2016-11-13 22:08:25 +0100

    udpate

--- public/cli/tabellerna.php
+++ public/cli/tabellerna.php
@@ -16,41 +16,81 @@ report($results);
 
 function run_multi_ai(){
 	echo "\nMultiplication AI mode";
-	echo "\n quits when 5 correct in a row";
-	echo "\n re-tests failing factors";
+	echo "\n Test ends when quote < 5";
+	echo "\n Else progresses for ever ";
+	echo "\n All questions have delay timer for report afterwards";
+	echo "\n todo delay time max 3 seconds to progress";
+	echo "\n else levels down";
+
+	#todo ad AI factor progression (never ending iterations?)
+
+	#For progression
+	#$maxfactor = 2; #
+	
+	#Level +- 1 is for training round a level
+	$level = 2; # Max factor is level
+	$level_diff = 2; #scatter +- 2  not used yet
+	$level_quota = 0; # +- correctness frequency ++ for right -- for fail
+	#Each iteration quota is > 2 level is incremented
+
+
+
+
 
 	$timer = array_sum(explode(' ', microtime())); #timer
 	
-	$num_questions = 4;
+	$num_questions = 10;
 	$right = 0;
 	$wrong = 0;
 	$hardfactors = array();
 
-	for ($i = 1;$i < $num_questions +1  ;$i++){
+	#for ($i = 1;$i < $num_questions +1  ;$i++){ OLD for NON AI
+	$i=0;
+	while($level_quota > -5 ){
+		$i++;
 		$timer_q = array_sum(explode(' ', microtime())); #timer
 
-		$a = rand(2,9);
-		$b = rand(2,9);
+		if($level_quota > 2){
+			$level++;
+			$level_quota = 0; # reset
+		}
+
+		if($level_quota <= -2){
+			#echo "LOW quite!";
+			$level--;
+			$level_quota = 0; # reset
+		}
+
+		
+
+		$a = rand(1,$level);
+		$b = rand(1,$level);
+
+		
+
 
 		if(count($hardfactors) > 0){
-			echo "\nQuestion[$i/$num_questions] Hardfactors[".count($hardfactors)."]  ->";
-			foreach ($hardfactors as $key => $value) {
-				echo " $value";
-			}
-			$a = array_pop($hardfactors); #move a faktor from array to next question
+			$b = array_pop($hardfactors); #move a faktor from array to next question
 		}
 
 		$svar = $a*$b;
 
+
+		echo "\nQuestion[$i] Level[$level] Quota[$level_quota] Hardfactors[".count($hardfactors)."]  ->";
+		foreach ($hardfactors as $key => $value) {
+			echo " $value";
+		}
 		echo "\n$a x $b = ";
 		$input = trim(input());
 		#echo "\n du sa ".$input;
 
 		if( $input == $svar){
-			#echo " yes!";
+			# CORRECT ANSWER
+			$level_quota++;
 			$right++;
 			$results[$i]['result'] = 'Correct';
 		}else{
+			$level_quota--;
 			$wrong++;
 			echo " Fail.. ($svar)";
 			$results[$i]['result'] = 'Fail';
@@ -58,6 +98,11 @@ function run_multi_ai(){
 			$hardfactors[]=$b;
 			$num_questions = $num_questions + 2; #add another question to the loop
 
+		}
+
+		#dont allow test to end if factors are left
+		if( count($hardfactors) > 0) {
+			$num_questions++;
 		}
 		
 		$results[$i]['question'] = "$a x $b = $svar ($input)";
